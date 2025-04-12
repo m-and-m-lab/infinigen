b268200cad0062d99d4c31132f3d46bd27832426
Jacob Skwirsk <skwirskj@umich.edu>
2025-04-12 14:34:21 -0400

    trying a different config and task for generating scene

--- generate_apartment.sh
+++ generate_apartment.sh
@@ -1,2 +1,2 @@
-python -m infinigen_examples.generate_indoors --seed 0 --task coarse --output_folder infinigen_outputs/indoors/coarse_multiroom -g fast_solve.gin singleroom.gin -p compose_indoors.terrain_enabled=False &&
+python -m infinigen_examples.generate_indoors --seed 0 --task populate --output_folder infinigen_outputs/indoors/coarse_multiroom -g fast_solve.gin singleroom.gin -p compose_indoors.terrain_enabled=False &&
 python -m infinigen.tools.export --input_folder infinigen_outputs/indoors/coarse_multiroom --output_folder infinigen_outputs/omniverse/coarse_multiroom -f usdc -r 1024 --omniverse
